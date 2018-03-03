63601d56ed4000a6b1d5a534716a70fcb8f45ade
linedeveloptest <linedeveloptest@gmail.com>
2018-03-04 00:15:49 +0800

    Add air quility

--- linebot/index.js
+++ linebot/index.js
@@ -1,5 +1,13 @@
 const linebot = require('linebot');
 const express = require('express');
+const rp = require('request-promise');
+const bodyParser = require('body-parser');
+
+const SITE_NAME = '西屯';
+const aqiOpt = {
+    uri: "http://opendata2.epa.gov.tw/AQI.json",
+    json: true
+}; 
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
@@ -7,22 +15,72 @@ const bot = linebot({
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
+function readAQI(repos){
+    let data;
+    
+    for (i in repos) {
+        if (repos[i].SiteName == SITE_NAME) {
+            data = repos[i];
+            break;
+        }
+    }
+
+    return data;
+}
+
 const app = express();
+app.set('view engine', 'ejs');
 
 const linebotParser = bot.parser();
 
 app.get('/',function(req,res){
-    res.send('Hello World!');
+    rp(aqiOpt)
+    .then(function (repos) {
+        res.render('index', {AQI:readAQI(repos)});
+    })
+    .catch(function (err) {
+		res.send("無法取得空氣品質資料～");
+    });
 });
 
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
-	event.reply(event.message.text).then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	});
+	switch (event.message.type) {
+		case 'text':
+			switch (event.message.text) {
+				case '空氣':
+					let data;
+					rp(aqiOpt)
+					.then(function (repos) {
+						data = readAQI(repos);
+						event.reply(data.County + data.SiteName +
+						'\n\nPM2.5指數：'+ data["PM2.5_AVG"] + 
+					    '\n狀態：' + data.Status);
+					})
+					.catch(function (err) {
+						event.reply('無法取得空氣品質資料～');
+					});
+					break;
+
+				case 'Me':
+					event.source.profile().then(function (profile) {
+						return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+					});
+					break;
+			}
+			break;
+		case 'sticker':
+			event.reply({
+				type: 'sticker',
+				packageId: 1,
+				stickerId: 1
+			});
+			break;
+		default:
+			event.reply('Unknow message: ' + JSON.stringify(event));
+			break;
+	}
 });
 
 app.listen(process.env.PORT || 80, function () {
